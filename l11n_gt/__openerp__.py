545ca04421d7342919fe52cc01f2477188f1fd49
Miguel Chuga <miguelchuga@gmail.com>
2015-06-12 19:08:54 -0600

    1ra subida de la localizacion

---
+++ l11n_gt/__openerp__.py
@@ -0,0 +1,20 @@
+# -*- encoding: utf-8 -*-
+
+{
+    'name': 'Guatemala - Accounting',
+    'version': '3.0',
+    'category': 'Localization/Account Charts',
+    'description': "Crea la nomenclatura contable de Guatemala.",
+    'author': 'Francisco Lira',
+    'website': 'http://solucioninternet.net/',
+    'depends': ['base', 'account', 'account_chart'],
+    'data': [
+        'account_types.xml',
+        'account_chart.xml',
+        'account_tax.xml',
+        'l10n_gt_base.xml',
+    ],
+    'demo': [],
+    'installable': True,
+}
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
